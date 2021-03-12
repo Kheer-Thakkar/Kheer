dbbacba5032eda165450e7ce3dd43600ec04ad3b
Kheer-Thakkar <77600784+Kheer-Thakkar@users.noreply.github.com>
2021-03-12 22:39:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,155 +1,29 @@
-canvas = document.getElementById("myCanvas");
-ctx=canvas.getContext("2d");
-car1_width = 120;
-car1_height = 70;
-car1_image = "https://i.postimg.cc/YqdnnNX1/car1.png";
-car1_x = 10;
-car1_y = 10;
+var menu_list_array= ["Greek Pizza",
+"Barbecue Pineapple, Jalapeño and Feta Pizza",
+"Part-Skim Mozzarella",
+"European cheese pizza",
+"Itlian pizza",];
 
-car2_width = 120;
-car2_height = 70;
-car2_image = "https://i.postimg.cc/YqdnnNX1/car1.png";
-car2_x = 10;
-car2_y = 100;
-background_image = "https://i.postimg.cc/bv5d35nK/racing.jpg";
-
-function add(){
-    background_imgTag = new Image();
-    background_imgTag.onload=uploadBackground;
-    background_imgTag.src = background_image;
-    
-    car1_imgTag=new Image();
-   car1_imgTag.onload = uploadcar1;
-   car1_imgTag.src = car1_image;
-    
-
-    car2_imgTag=new Image();
-    car2_imgTag.onload = uploadcar2;
-    car2_imgTag.src = car2_image;
-     }
-
-    function uploadBackground(){
-        ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.height);
+function getmenu(){
+    var htmldata;
+    htmldata="<ol class='menulist'>"
+    menu_list_array.sort();
+    for(var i=0;i<menu_list_array.length;i++){
+        htmldata=htmldata+'<li>' +menu_list_array[i] + '</li>'
     }
-    function uploadcar1(){
-       ctx.drawImage(car1_imgTag,car1_x,car1_y,car1_width,car1_height);
-   
+    htmldata=htmldata+"</ol>"
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_item(){
+    var htmldata;
+    var item =document.getElementById("add_item").Value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata="<section class='cards'>"
+    for(var i=0;i<menu_list_array.length;i++){
+        htmldata=htmldata+ '<div class="card">' +'<img src="images/pizzaImg.png"/>' + menu_list_array[i] + '</div>'
     }
-
-    function uploadcar2(){
-        ctx.drawImage(car2_imgTag,car2_x,car2_y,car2_width,car2_height);
-    }
-
-    window.addEventListener("keydown",my_keydown);
-
-    function my_keydown(e){
-        keyPressed = e.keyCode;
-        console.log(keyPressed);
-        if(keyPressed == '38') { 
-            up();
-             console.log("up");
-             }
-             if(keyPressed == '40')
-              {
-                   down(); 
-                   console.log("down");
-                 }
-                  if(keyPressed == '37') {
-                       left();
-                        console.log("left");
-                     } 
-                     if(keyPressed == '39') {
-                          right(); 
-                          console.log("right");
-                         }
-                         if(keyPressed == '87'){
-                         car2_up();
-                         console.log("key w");
-                         }
-                         if(keyPressed == '83'){
-                            car2_down();
-                            console.log("key s");
-                            }
-                            if(keyPressed == '65'){
-                                car2_left();
-                                console.log("key a");
-                                }
-                                if(keyPressed == '68'){
-                                    car2_right();
-                                    console.log("key d");
-                                    }
-                        }
-
-        
-
-        function up() {
-            if(car1_y >=0) {
-                car1_y = car1_y - 10;
-                  console.log("When up arrow is pressed, x = " + car1_x + " | y = " + car1_y);
-                   uploadBackground();
-                    uploadcar1(); }
-                  } 
-
-                  function down() {
-                    if(car1_y <=500) { 
-                         car1_y = car1_y + 10; console.log("When down arrow is pressed, x = " + car1_x + " | y = " + car1_y);
-                          uploadBackground();
-                           uploadcar1();
-                        } 
-                       } 
-
-                       function left() 
-                       { 
-                            if(car1_x >= 0) { 
-                                car1_x = car1_x - 10; console.log("When left arrow is pressed, x = " + car1_x + " | y = " + car1_y); 
-                                 uploadBackground(); 
-                                 uploadcar1();
-                             } 
-                            } 
-
-                            function right()
-                          {
-                                if(car1_x <= 700) {
-                                      car1_x = car1_x + 10;
-                                       console.log("When right arrow is pressed, x = " + car1_x + " | y = " + car1_y); 
-                                       uploadBackground(); 
-                                       uploadcar1();
-                                    }
-                                    }
-
-                                    function car2_up()
-                                     { 
-                                         if(car2_y >=0) {
-                                              car2_y = car2_y - 10; 
-                                              console.log("When up arrow is pressed, x = " + car2_x + " | y = " +car2_y); 
-                                              uploadBackground();
-                                               uploadcar1();
-                                                uploadcar2();
-                                             } }
-                                              function car2_down()
-                                               {
-                                                    if(car1_y <=500) {
-                                                         car2_y =car2_y+ 10; 
-                                                         console.log("When down arrow is pressed, x = " + car2_x + " | y = " +car2_y);
-                                                          uploadBackground(); 
-                                                          uploadcar1(); 
-                                                          uploadcar2();
-                                                         } } 
-                                                         function car2_left() 
-                                                         { 
-                                                             if(car2_x >= 0) {
-                                                                  car2_x =car2_x - 10;
-                                                                   console.log("When left arrow is pressed, x = " + car2_x + " | y = " +car2_y);
-                                                                    uploadBackground();
-                                                                     uploadcar1(); 
-                                                                     uploadcar2();
-                                                                     } }
-                                                                      function car2_right()
-                                                                       {
-                                                                            if(car2_x <= 700) {
-                                                                                 car2_x =car2_x + 10;
-                                                                                  console.log("When right arrow is pressed, x = " + car2_x + " | y = " +car2_y);
-                                                                                   uploadBackground(); 
-                                                                                   uploadcar1(); 
-                                                                                   uploadcar2();
-                                                                                 } }
+    htmldata=htmldata+item"</section>"
+    document.getElementById("display_addmenu").innerHTML = htmldata;
+}
