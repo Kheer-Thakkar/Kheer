8554c010e8f1cf592cfe1fe67455afd771d5db04
Kheer-Thakkar <77600784+Kheer-Thakkar@users.noreply.github.com>
2021-03-22 23:03:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,155 +1,88 @@
-canvas = document.getElementById("myCanvas");
-ctx=canvas.getContext("2d");
-car1_width = 120;
-car1_height = 70;
-car1_image = "https://i.postimg.cc/YqdnnNX1/car1.png";
-car1_x = 10;
-car1_y = 10;
+var canvas=new fabric.Canvas("myCanvas");
+ player_x=10;
+ player_y=10;
+ block_image_width=30;
+ block_image_height=30;
+ var player_object="";
+ var block_image_object="";
 
-car2_width = 120;
-car2_height = 70;
-car2_image = "https://i.postimg.cc/YqdnnNX1/car1.png";
-car2_x = 10;
-car2_y = 100;
-background_image = "https://i.postimg.cc/bv5d35nK/racing.jpg";
-
-function add(){
-    background_imgTag = new Image();
-    background_imgTag.onload=uploadBackground;
-    background_imgTag.src = background_image;
-    
-    car1_imgTag=new Image();
-   car1_imgTag.onload = uploadcar1;
-   car1_imgTag.src = car1_image;
-    
-
-    car2_imgTag=new Image();
-    car2_imgTag.onload = uploadcar2;
-    car2_imgTag.src = car2_image;
-     }
-
-    function uploadBackground(){
-        ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.height);
-    }
-    function uploadcar1(){
-       ctx.drawImage(car1_imgTag,car1_x,car1_y,car1_width,car1_height);
-   
-    }
-
-    function uploadcar2(){
-        ctx.drawImage(car2_imgTag,car2_x,car2_y,car2_width,car2_height);
-    }
-
-    window.addEventListener("keydown",my_keydown);
-
-    function my_keydown(e){
-        keyPressed = e.keyCode;
-        console.log(keyPressed);
-        if(keyPressed == '38') { 
-            up();
-             console.log("up");
-             }
-             if(keyPressed == '40')
-              {
-                   down(); 
-                   console.log("down");
-                 }
-                  if(keyPressed == '37') {
-                       left();
-                        console.log("left");
-                     } 
-                     if(keyPressed == '39') {
-                          right(); 
-                          console.log("right");
-                         }
-                         if(keyPressed == '87'){
-                         car2_up();
-                         console.log("key w");
-                         }
-                         if(keyPressed == '83'){
-                            car2_down();
-                            console.log("key s");
-                            }
-                            if(keyPressed == '65'){
-                                car2_left();
-                                console.log("key a");
-                                }
-                                if(keyPressed == '68'){
-                                    car2_right();
-                                    console.log("key d");
-                                    }
-                        }
-
+ function player_update(){
+    fabric.Image.fromURL("https://drive.google.com/file/d/1-hMfzjM02r_h02NDgkQk9WmFTB-ZErBt/view?usp=sharing",function(Img){
+        player_object=Img;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top:player_y,
+            left:player_x
         
+    });
 
-        function up() {
-            if(car1_y >=0) {
-                car1_y = car1_y - 10;
-                  console.log("When up arrow is pressed, x = " + car1_x + " | y = " + car1_y);
-                   uploadBackground();
-                    uploadcar1(); }
-                  } 
+    canvas.add(player_object);
 
-                  function down() {
-                    if(car1_y <=500) { 
-                         car1_y = car1_y + 10; console.log("When down arrow is pressed, x = " + car1_x + " | y = " + car1_y);
-                          uploadBackground();
-                           uploadcar1();
-                        } 
-                       } 
+});
+}
+function new_image(get_image){
+    fabric.Image.fromURL(get_image,function(Img){
+        block_image_object=Img;
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_object.scaleToHeight(block_image_height);
+        block_image_object.set({
+            top:player_y,
+            left:player_x
+        
+    });
 
-                       function left() 
-                       { 
-                            if(car1_x >= 0) { 
-                                car1_x = car1_x - 10; console.log("When left arrow is pressed, x = " + car1_x + " | y = " + car1_y); 
-                                 uploadBackground(); 
-                                 uploadcar1();
-                             } 
-                            } 
+    canvas.add(block_image_object);
 
-                            function right()
-                          {
-                                if(car1_x <= 700) {
-                                      car1_x = car1_x + 10;
-                                       console.log("When right arrow is pressed, x = " + car1_x + " | y = " + car1_y); 
-                                       uploadBackground(); 
-                                       uploadcar1();
-                                    }
-                                    }
+});
+}
+if (keyPressed == '70'){
+    new_image("https://i.postimg.cc/hGnyTPLB/ironman-face.png");
+    console.log("f");
+    
+ }
 
-                                    function car2_up()
-                                     { 
-                                         if(car2_y >=0) {
-                                              car2_y = car2_y - 10; 
-                                              console.log("When up arrow is pressed, x = " + car2_x + " | y = " +car2_y); 
-                                              uploadBackground();
-                                               uploadcar1();
-                                                uploadcar2();
-                                             } }
-                                              function car2_down()
-                                               {
-                                                    if(car1_y <=500) {
-                                                         car2_y =car2_y+ 10; 
-                                                         console.log("When down arrow is pressed, x = " + car2_x + " | y = " +car2_y);
-                                                          uploadBackground(); 
-                                                          uploadcar1(); 
-                                                          uploadcar2();
-                                                         } } 
-                                                         function car2_left() 
-                                                         { 
-                                                             if(car2_x >= 0) {
-                                                                  car2_x =car2_x - 10;
-                                                                   console.log("When left arrow is pressed, x = " + car2_x + " | y = " +car2_y);
-                                                                    uploadBackground();
-                                                                     uploadcar1(); 
-                                                                     uploadcar2();
-                                                                     } }
-                                                                      function car2_right()
-                                                                       {
-                                                                            if(car2_x <= 700) {
-                                                                                 car2_x =car2_x + 10;
-                                                                                  console.log("When right arrow is pressed, x = " + car2_x + " | y = " +car2_y);
-                                                                                   uploadBackground(); 
-                                                                                   uploadcar1(); 
-                                                                                   uploadcar2();
-                                                                                 } }
+ if (keyPressed == '70'){
+    new_image("https://i.postimg.cc/FscwL6M0/spiderman-body.png");
+    console.log("b");
+    
+ }
+
+ if (keyPressed == '70'){
+    new_image("https://i.postimg.cc/KzF6GDqt/hulk-legs.png");
+    console.log("l");
+    
+ }
+
+ if (keyPressed == '70'){
+    new_image("https://i.postimg.cc/8s8BGtwS/thor-right-hand.png");
+    console.log("r");
+    
+ }
+
+ if (keyPressed == '70'){
+    new_image("https://i.postimg.cc/rw7ckW29/captain-america-left-hand.png");
+    console.log("h");
+    
+ }
+
+ if (keyPressed == '38'){
+    up();
+    console.log("up");
+
+ }
+ if (keyPressed == '40'){
+   down();
+   console.log("down");
+   
+}
+if (keyPressed == '37'){
+   left();
+   console.log("left");
+   
+}
+if (keyPressed == '39'){
+   right();
+   console.log("right");
+   
+}
