4442de29982b5265f96c35e1a8fbd197d73f6b04
Kheer-Thakkar <77600784+Kheer-Thakkar@users.noreply.github.com>
2021-05-15 02:57:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,88 +1,76 @@
-var canvas=new fabric.Canvas("myCanvas");
- player_x=10;
- player_y=10;
- block_image_width=30;
- block_image_height=30;
- var player_object="";
- var block_image_object="";
-
- function player_update(){
-    fabric.Image.fromURL("https://drive.google.com/file/d/1-hMfzjM02r_h02NDgkQk9WmFTB-ZErBt/view?usp=sharing",function(Img){
-        player_object=Img;
-        player_object.scaleToWidth(150);
-        player_object.scaleToHeight(140);
-        player_object.set({
-            top:player_y,
-            left:player_x
-        
-    });
-
-    canvas.add(player_object);
-
-});
-}
-function new_image(get_image){
-    fabric.Image.fromURL(get_image,function(Img){
-        block_image_object=Img;
-        block_image_object.scaleToWidth(block_image_width);
-        block_image_object.scaleToHeight(block_image_height);
-        block_image_object.set({
-            top:player_y,
-            left:player_x
-        
-    });
-
-    canvas.add(block_image_object);
-
-});
-}
-if (keyPressed == '70'){
-    new_image("https://i.postimg.cc/hGnyTPLB/ironman-face.png");
-    console.log("f");
+prediction_1="";
+prediction_2="";
+      
+  Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'jpeg',
+    png_quality:90
+  });
+  camera=document.getElementById("camera");
+  Webcam.attach('#camera');
+  
+  function take_snapshot()
+  {
+      Webcam.snap(function(data_uri) {
+          document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+      });
+  }
+  
+  
+console.log('ml5 version',ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/3KNeB-U7X/model.json',modelLoaded);
+function modelLoaded()
+ {
+    console.log('model Loaded!');
     
  }
-
- if (keyPressed == '70'){
-    new_image("https://i.postimg.cc/FscwL6M0/spiderman-body.png");
-    console.log("b");
-    
- }
-
- if (keyPressed == '70'){
-    new_image("https://i.postimg.cc/KzF6GDqt/hulk-legs.png");
-    console.log("l");
-    
- }
-
- if (keyPressed == '70'){
-    new_image("https://i.postimg.cc/8s8BGtwS/thor-right-hand.png");
-    console.log("r");
-    
- }
-
- if (keyPressed == '70'){
-    new_image("https://i.postimg.cc/rw7ckW29/captain-america-left-hand.png");
-    console.log("h");
-    
- }
-
- if (keyPressed == '38'){
-    up();
-    console.log("up");
+ function speak(){
+     var synth=window.speechSynthesis;
+     speak_data_1=" first prediction is"+prediction_1;yu
+     speak_data_2=" second prediction is"+prediction_2;
+     var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+     synth.speak(utterThis);
 
  }
- if (keyPressed == '40'){
-   down();
-   console.log("down");
-   
-}
-if (keyPressed == '37'){
-   left();
-   console.log("left");
-   
-}
-if (keyPressed == '39'){
-   right();
-   console.log("right");
-   
-}
+ function check() {
+    img = document.getElementById('selfie_image');
+     classifier.classify(img, gotResult); 
+    } 
+    function gotResult(error, results)
+     { 
+       if (error) { 
+         console.error(error);
+     } 
+     else
+      { 
+       console.log(results);
+       document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name1").innerHTML = results[1].label;
+        prediction_1=results[0].label;
+        prediction_2=results[1].label;
+        speak();
+        if(results[0].label == "happy") 
+        { 
+          document.getElementById("update_emoji").innerHTML = "&#128522;"; 
+        } 
+        if(results[0].label == "sad")
+         { 
+           document.getElementById("update_emoji").innerHTML = "&#128532;";
+           } 
+           if(results[0].label == "angry") 
+           {
+              document.getElementById("update_emoji").innerHTML = "&#128548;";
+             }
+              if(results[1].label == "happy")
+               {
+                  document.getElementById("update_emoji2").innerHTML = "&#128522;";
+                 }
+                  if(results[1].label == "sad")
+                   { 
+                     document.getElementById("update_emoji2").innerHTML = "&#128532;";
+                     }
+                      if(results[1].label == "angry") 
+                      { 
+          document.getElementById("update_emoji2").innerHTML = "&#128548;";
+       }} }
